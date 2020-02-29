8e77ecd770bb9485e32bdf83c94a21aa72d55b9f
ElonH <elonhhuang@gmail.com>
2020-02-29 17:41:11 +0800

    deploy image support

--- scripts/main-build.sh
+++ scripts/main-build.sh
@@ -37,8 +37,12 @@ argsContains () {
 	return 1
 }
 
-if (argsContains "--sdk" ) && (argsContains "--build-sdk"); then
-	exiterr "You can't select both --build-sdk and --sdk"
+count=0
+(argsContains "--sdk" ) && ((count++))
+(argsContains "--build-sdk") && ((count++))
+(argsContains "--image") && ((count++))
+if [[ count -gt 1 ]]; then
+	exiterr "--build-sdk --sdk --image only one can be chosen."
 fi
 
 # main program
@@ -50,6 +54,7 @@ if ( argsContains "--help" );then
 	--feeds			update and install feeds[default is disable]
 	--build-sdk		disable extra default package (useful for build SDK)[default is disable]
 					do not add addon packages (useful for build SDK)[default is disable]
+	--image			build image using pre-build packages
 	"
 	exit 0
 fi
@@ -58,16 +63,38 @@ fi
 if ( argsContains "--sdk" );then
     SOURCE_BASE_PATH="${SOURCE_BASE_PATH}_sdk"
 fi
+if ( argsContains "--image" );then
+    SOURCE_BASE_PATH="${SOURCE_BASE_PATH}_ib"
+fi
 cd "$SOURCE_BASE_PATH" || exiterr "path '$SOURCE_BASE_PATH' not exist"
 
-# add base feeds (only for SDK)
-if ( argsContains "--sdk" );then
+if ( argsContains "--image" );then
+	REPO_BASE_PATH="$SCRIPT_ABS_PATH/${SOURCE_NAME}_sdk/bin"
+	[ -d "$REPO_BASE_PATH" ] || exiterr "repo path '$REPO_BASE_PATH' not exist"
+	REPOS_PATH=$(find "$REPO_BASE_PATH/packages" -name "Packages")
+	FEEDS_CONF="src imagebuilder file:packages
+"
+	for item in ${REPOS_PATH[*]}
+	do
+		item=${item%/*}
+		name=${item##*/}
+		# echo $item
+		# echo $name
+		FEEDS_CONF+="src $name file://$item
+"
+	done
+	echo "$FEEDS_CONF">repositories.conf
+	echo "$FEEDS_CONF"
+	exit 0
+elif ( argsContains "--build-sdk" );then
+	echo "${FEEDS_CONF}">feeds.conf
+elif ( argsContains "--sdk" );then
+	# add base feeds (only for SDK)
 	FEEDS_CONF+="
 src-link base $SOURCE_BASE_PATH
 "
+	echo "${FEEDS_CONF}">feeds.conf
 fi
-echo "${FEEDS_CONF}">feeds.conf
-
 
 if ( argsContains "--feeds" );then
     ./scripts/feeds update -a && ./scripts/feeds install -a
@@ -95,6 +122,7 @@ CONFIG_CCACHE=y
 $USER_PACK_CONF
 "
 fi
+
 
 PACK_CONF+="$BASE_PACK_CONF $BUILD_SDK_PACK_CONF $SDK_PACK_CONF"
 
