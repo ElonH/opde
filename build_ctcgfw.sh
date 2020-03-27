da4d12920830b754c670e855e05abda39d53738d
ElonH <elonhhuang@gmail.com>
2020-03-27 23:07:47 +0800

    cancel default packages

--- build_ctcgfw.sh
+++ build_ctcgfw.sh
@@ -24,6 +24,27 @@ $(IMG_SETTING)
 $(ENABLE_LOG)
 CONFIG_CCACHE=y
 
+# cancel default packages
+CONFIG_PACKAGE_luci-app-ddns=n
+CONFIG_PACKAGE_luci-app-sqm=n
+CONFIG_PACKAGE_luci-app-upnp=n
+CONFIG_PACKAGE_luci-app-adbyby-plus=n
+CONFIG_PACKAGE_luci-app-autoreboot=n
+CONFIG_PACKAGE_luci-app-filetransfer=n
+CONFIG_PACKAGE_luci-app-vsftpd=n
+CONFIG_PACKAGE_luci-app-ssr-plus=n
+CONFIG_PACKAGE_luci-app-unblockmusic=n
+CONFIG_PACKAGE_luci-app-arpbind=n
+CONFIG_PACKAGE_luci-app-vlmcsd=n
+CONFIG_PACKAGE_luci-app-wol=n
+CONFIG_PACKAGE_luci-app-ramfree=n
+CONFIG_PACKAGE_luci-app-sfe=n
+CONFIG_PACKAGE_luci-app-nlbwmon=n
+CONFIG_PACKAGE_luci-app-accesscontrol=n
+CONFIG_PACKAGE_luci-app-cpufreq=n
+CONFIG_PACKAGE_ddns-scripts_aliyun=n
+CONFIG_PACKAGE_ddns-scripts_dnspod=n
+
 CONFIG_PACKAGE_kmod-batman-adv=n
 CONFIG_PACKAGE_kmod-mt_wifi=n
 CONFIG_PACKAGE_kmod-rtl8821cu=n
