f4e3f2df03dcd2d36db26fcfa7819fa32e420848
ElonH <elonhhuang@gmail.com>
2020-04-10 15:43:05 +0800

    ipq40xx: init

--- build_ctcgfw.sh
+++ build_ctcgfw.sh
@@ -18,13 +18,13 @@ src-link telephony ${SCRIPT_ABS_PATH}/feeds/openwrt/telephony
 # basic configuration whatever build SDK, build packages or develepment
 function base_pack_conf {
 BASE_PACK_CONF="
-$(TARGET_X86_64)
+$(TARGET_ipq40xx_generic)
 
 $(GENERAL_SETTING)
 
-CONFIG_PACKAGE_kmod-batman-adv=n
-CONFIG_PACKAGE_kmod-mt_wifi=n
-CONFIG_PACKAGE_kmod-rtl8821cu=n
+CONFIG_PACKAGE_kmod-ath10k-ct-smallbuffers=m
+
+CONFIG_PACKAGE_ath10k-firmware-qca4019-ct=n
 
 CONFIG_PACKAGE_luci-app-ddns=n
 CONFIG_PACKAGE_luci-app-sqm=n
--- build_latest.sh
+++ build_latest.sh
@@ -18,12 +18,13 @@ src-link telephony ${SCRIPT_ABS_PATH}/feeds/openwrt/telephony
 # basic configuration whatever build SDK, build packages or develepment
 function base_pack_conf {
 BASE_PACK_CONF="
-$(TARGET_X86_64)
+$(TARGET_ipq40xx_generic)
 
 $(GENERAL_SETTING)
 
 CONFIG_PACKAGE_kmod-rtl8821cu=n
-CONFIG_PACKAGE_kmod-dahdi=n
+CONFIG_PACKAGE_kmod-ath10k-ct=n
+
 "
 }
 
