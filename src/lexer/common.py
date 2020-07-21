f267551ed49a9156a507721620cb31343e030d49
ElonH <elonhhuang@gmail.com>
2020-07-22 02:07:32 +0800

    lexer: enable save tokens for dev

--- src/lexer/common.py
+++ src/lexer/common.py
@@ -23,9 +23,17 @@ class InfoLexer():
     def testSlient(self, row_data: str):
         'Test it output'
         self.lexer.input(row_data)
-        # with open('/mnt/A/opde/laxer.out', 'w') as f:
         while True:
             tok = self.lexer.token()
             if not tok:
                 break
-            # f.write('%s\n' % tok)
+
+    def saveTo(self, row_data: str, path: str):
+        'Save tokens to somewhere'
+        self.lexer.input(row_data)
+        with open(path, 'w') as f:
+            while True:
+                tok = self.lexer.token()
+                if not tok:
+                    break
+                f.write('%s\n' % tok)
