6708b5980011089c61b8d8a8f5a41eeb293632d9
ElonH <elonhhuang@gmail.com>
2020-07-27 01:29:59 +0800

    dumper: as back to origin syntax

---
+++ src/dumper/kconfig.py
@@ -0,0 +1,16 @@
+
+def KconfigDumper(kconfigAst: object, indent='\t'):
+    '''
+    transform kconfigAst ack to kconfig
+    '''
+    ans = []
+    for item in kconfigAst:
+        ans_item = []
+        ans_item.append('%s %s' % (item['type'], item['sym']))
+        ans_item.append('%s%s' % (indent, item['value-type']))
+        if item['value-type'] != 'string':
+            ans_item.append('%sdefault %s' % (indent, item['default']))
+        else:
+            ans_item.append('%sdefault "%s"' % (indent, item['default']))
+        ans.append('\n'.join(ans_item))
+    return '\n\n'.join(ans)
