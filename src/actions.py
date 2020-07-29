e60644b92970d93679738cf251756362dc25c278
ElonH <elonhhuang@gmail.com>
2020-07-29 21:44:00 +0800

    format code

--- src/actions.py
+++ src/actions.py
@@ -2,13 +2,14 @@ import json
 from pprint import pprint
 from pathlib import Path
 import yaml
+import re
 
 
 def str_presenter(dumper, data):
     def is_multiline(s): return len(s.splitlines()) > 1
     # print(data, is_multiline(data))
     if is_multiline(data):  # check for multiline string
-        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+        return dumper.represent_scalar('tag:yaml.org,2002:str', re.sub(r'\n[ ]+', '\n', data), style='|')
     return dumper.represent_scalar('tag:yaml.org,2002:str', data)
 
 
@@ -65,7 +66,12 @@ class WorkFlow:
         return '\n'.join(ans)
 
     @classmethod
-    def _in_var_cmd(cls, stepid: str, key: str):
+    def _out_var(cls, jobid: str, key: str):
+        'get variable from other job'
+        return '${{needs.%s.outputs.%s}}' % (jobid, key)
+
+    @classmethod
+    def _in_var(cls, stepid: str, key: str):
         'get variable from same job'
         return '${{steps.%s.outputs.%s}}' % (stepid, key)
 
@@ -87,9 +93,9 @@ class WorkFlow:
         job_apt = self._gen_empty_job()
         stps: list = self._gen_empty_steps()
         job_apt['outputs'] = {
-            "dateDot": self._in_var_cmd('var', 'dateDot'),
-            "dateDash": self._in_var_cmd('var', 'dateDash'),
-            "tag": self._in_var_cmd('var', 'tag'),
+            "dateDot": self._in_var('var', 'dateDot'),
+            "dateDash": self._in_var('var', 'dateDash'),
+            "tag": self._in_var('var', 'tag'),
         }
         stps.extend([
             # {'run': '[[ "${{secrets.RELEASE_TOKEN}}" ]] || false'},
@@ -103,54 +109,56 @@ class WorkFlow:
             },
             self._gen_cache_step(
                 './cache/apt',
-                "apt-${{steps.var.outputs.dateDash}}-${{ hashFiles('./cache/apt.list.txt') }}",
+                "apt-%s-${{ hashFiles('./cache/apt.list.txt') }}" % self._in_var(
+                    'var', 'dateDash'),
                 'cache-apt'
             ),
             self._gen_cache_step(
                 './cache/python',
-                "python-${{steps.var.outputs.dateDash}}-${{ hashFiles('./poetry.lock') }}",
+                "python-%s-${{ hashFiles('./poetry.lock') }}" % self._in_var(
+                    'var', 'dateDash'),
                 'cache-python'
             ),
             {
                 'if': "steps.cache-apt.outputs.cache-hit != 'true' || steps.cache-python.outputs.cache-hit != 'true'",
                 'run': r'''
-docker rmi $(docker images -q)
-sudo -E apt-get remove -y --purge azure-cli ghc zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
-sudo -E apt-get update -y || ( sleep 1m && sudo -E apt-get update -y) || ( sleep 1m && sudo -E apt-get update -y)
-sudo -E apt install -y apt-offline
-APT_PACKS=($(tr '\n' ' ' < ./cache/apt.list.txt))
-echo "opde install packages list:"
-echo ${APT_PACKS[@]}
-# https://blog.sleeplessbeastie.eu/2014/01/30/how-to-manage-packages-on-an-off-line-debian-system/
-sudo -E apt-key exportall | sudo -E gpg --no-default-keyring --import --keyring /etc/apt/trusted.gpg
-sudo -E apt-offline set ./cache/apt/opde-apt.sig --update --upgrade --install-packages ${APT_PACKS[@]}
-apt-offline get cache/apt/opde-apt.sig --bundle ./cache/apt/opde-bundle.zip -t $(($(nproc)*2))
-echo "testing intall..."
-# --allow-unauthenticated
-sudo -E apt-offline install ./cache/apt/opde-bundle.zip --skip-bug-reports --skip-changelog
-sudo -E apt-get upgrade
-sudo -E apt-get install ${APT_PACKS[@]}
-'''
+                    docker rmi $(docker images -q)
+                    sudo -E apt-get remove -y --purge azure-cli ghc zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+                    sudo -E apt-get update -y || ( sleep 1m && sudo -E apt-get update -y) || ( sleep 1m && sudo -E apt-get update -y)
+                    sudo -E apt install -y apt-offline
+                    APT_PACKS=($(tr '\n' ' ' < ./cache/apt.list.txt))
+                    echo "opde install packages list:"
+                    echo ${APT_PACKS[@]}
+                    # https://blog.sleeplessbeastie.eu/2014/01/30/how-to-manage-packages-on-an-off-line-debian-system/
+                    sudo -E apt-key exportall | sudo -E gpg --no-default-keyring --import --keyring /etc/apt/trusted.gpg
+                    sudo -E apt-offline set ./cache/apt/opde-apt.sig --update --upgrade --install-packages ${APT_PACKS[@]}
+                    apt-offline get cache/apt/opde-apt.sig --bundle ./cache/apt/opde-bundle.zip -t $(($(nproc)*2))
+                    echo "testing intall..."
+                    # --allow-unauthenticated
+                    sudo -E apt-offline install ./cache/apt/opde-bundle.zip --skip-bug-reports --skip-changelog
+                    sudo -E apt-get upgrade
+                    sudo -E apt-get install ${APT_PACKS[@]}
+                    '''
             },
             {
                 'if': "steps.cache-python.outputs.cache-hit != 'true'",
                 'run': r'''
-cd ./cache/python
-curl -SL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py
-# https://github.com/python-poetry/poetry/issues/2106#issuecomment-644471434
-sed -i 's/allowed_executables = \["python", "python3"\]/allowed_executables = \["python3", "python"\]/' get-poetry.py
-curl -SL https://github.com/python-poetry/poetry/releases/download/1.0.10/poetry-1.0.10-linux.tar.gz -o poetry.tar.gz
-python3 get-poetry.py --file poetry.tar.gz -y
-source $HOME/.poetry/env
-cd ${{ github.workspace }}
-python3 -m venv .venv
-source .venv/bin/activate
-poetry export -f requirements.txt -o ./cache/python/requirements.txt
-cd ./cache/python
-pip3 download -r requirements.txt -d wheelhouse
-# testing pip install
-pip3 install --no-index --find-links="./wheelhouse" -r requirements.txt
-'''
+                    cd ./cache/python
+                    curl -SL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py
+                    # https://github.com/python-poetry/poetry/issues/2106#issuecomment-644471434
+                    sed -i 's/allowed_executables = \["python", "python3"\]/allowed_executables = \["python3", "python"\]/' get-poetry.py
+                    curl -SL https://github.com/python-poetry/poetry/releases/download/1.0.10/poetry-1.0.10-linux.tar.gz -o poetry.tar.gz
+                    python3 get-poetry.py --file poetry.tar.gz -y
+                    source $HOME/.poetry/env
+                    cd ${{ github.workspace }}
+                    python3 -m venv .venv
+                    source .venv/bin/activate
+                    poetry export -f requirements.txt -o ./cache/python/requirements.txt
+                    cd ./cache/python
+                    pip3 download -r requirements.txt -d wheelhouse
+                    # testing pip install
+                    pip3 install --no-index --find-links="./wheelhouse" -r requirements.txt
+                    '''
             },
         ])
         job_apt['steps'] = stps
@@ -166,24 +174,24 @@ pip3 install --no-index --find-links="./wheelhouse" -r requirements.txt
             {
                 'env': {'DEBIAN_FRONTEND': 'noninteractive'},
                 'run': r'''
-docker rmi $(docker images -q)
-sudo -E apt-get -yq remove --purge azure-cli ghc zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
-sudo -E apt-get -yq update || ( sleep 1m && sudo -E apt-get update -y) || ( sleep 1m && sudo -E apt-get update -y)
-sudo -E apt-get -yq autoremove --purge
-sudo -E apt-get -yq clean
-sudo -E rm -rf /usr/share/dotnet /etc/mysql /etc/php
-sudo -E apt-get -yq install apt-offline
-APT_PACKS=($(tr '\n' ' ' < ./cache/apt.list.txt))
-# https://blog.sleeplessbeastie.eu/2014/01/30/how-to-manage-packages-on-an-off-line-debian-system/
-# --allow-unauthenticated
-sudo -E apt-offline install ./cache/apt/opde-bundle.zip --skip-bug-reports --skip-changelog
-sudo -E apt-get -yq upgrade
-sudo -E apt-get -yq install ${APT_PACKS[@]}
-sudo -E ln -sf /usr/bin/gcc-8 /usr/bin/gcc
-sudo -E ln -sf /usr/bin/g++-8 /usr/bin/g++
-# https://github.com/project-openwrt/openwrt-isco/issues/181
-sudo -E ln -sf /usr/include/asm-generic /usr/include/asm
-'''
+                    docker rmi $(docker images -q)
+                    sudo -E apt-get -yq remove --purge azure-cli ghc zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+                    sudo -E apt-get -yq update || ( sleep 1m && sudo -E apt-get update -y) || ( sleep 1m && sudo -E apt-get update -y)
+                    sudo -E apt-get -yq autoremove --purge
+                    sudo -E apt-get -yq clean
+                    sudo -E rm -rf /usr/share/dotnet /etc/mysql /etc/php
+                    sudo -E apt-get -yq install apt-offline
+                    APT_PACKS=($(tr '\n' ' ' < ./cache/apt.list.txt))
+                    # https://blog.sleeplessbeastie.eu/2014/01/30/how-to-manage-packages-on-an-off-line-debian-system/
+                    # --allow-unauthenticated
+                    sudo -E apt-offline install ./cache/apt/opde-bundle.zip --skip-bug-reports --skip-changelog
+                    sudo -E apt-get -yq upgrade
+                    sudo -E apt-get -yq install ${APT_PACKS[@]}
+                    sudo -E ln -sf /usr/bin/gcc-8 /usr/bin/gcc
+                    sudo -E ln -sf /usr/bin/g++-8 /usr/bin/g++
+                    # https://github.com/project-openwrt/openwrt-isco/issues/181
+                    sudo -E ln -sf /usr/include/asm-generic /usr/include/asm
+                    '''
             },
             self._gen_cache_step(
                 './cache/python',
@@ -191,14 +199,14 @@ sudo -E ln -sf /usr/include/asm-generic /usr/include/asm
             ),
             {
                 'run': r'''
-sed -i 's/allowed_executables = \["python", "python3"\]/allowed_executables = \["python3", "python"\]/' ./cache/python/get-poetry.py
-python3 ./cache/python/get-poetry.py --file ./cache/python/poetry.tar.gz -y
-source $HOME/.poetry/env
-sudo -E ln -sf $HOME/.poetry/bin/poetry /usr/bin/poetry
-python3 -m venv .venv
-source .venv/bin/activate
-pip3 install --no-index --find-links="./cache/python/wheelhouse" -r ./cache/python/requirements.txt
-'''
+                    sed -i 's/allowed_executables = \["python", "python3"\]/allowed_executables = \["python3", "python"\]/' ./cache/python/get-poetry.py
+                    python3 ./cache/python/get-poetry.py --file ./cache/python/poetry.tar.gz -y
+                    source $HOME/.poetry/env
+                    sudo -E ln -sf $HOME/.poetry/bin/poetry /usr/bin/poetry
+                    python3 -m venv .venv
+                    source .venv/bin/activate
+                    pip3 install --no-index --find-links="./cache/python/wheelhouse" -r ./cache/python/requirements.txt
+                    '''
             },
         ]
 
@@ -230,7 +238,7 @@ pip3 install --no-index --find-links="./cache/python/wheelhouse" -r ./cache/pyth
             },
             {
                 'run': 'poetry run python3 builder.py extract %s/logs %s ${{github.run_number}}' %
-                (self._in_var_cmd('sdk-var', 'openwrt'), self.db_path)
+                (self._in_var('sdk-var', 'openwrt'), self.db_path)
             },
             {'run': 'poetry run python3 builder.py check % s ${{github.run_number}})' % self.db_path},
             # TODO: workflow_dispatch
@@ -239,38 +247,38 @@ pip3 install --no-index --find-links="./cache/python/wheelhouse" -r ./cache/pyth
                     self.worker_num, self.db_path)
             },
             self._gen_upload_artifact_step(
-                'Kernel-Log', self._in_var_cmd(
+                'Kernel-Log', self._in_var(
                     'sdk-var', 'openwrt') + '/logs',
                 {'if': 'always()'}
             ),
             self._gen_upload_artifact_step(
-                'SDK', self._in_var_cmd('sdk-var', 'sdk-path')),
+                'SDK', self._in_var('sdk-var', 'sdk-path')),
             self._gen_upload_artifact_step(
-                'ImageBuilder', self._in_var_cmd('sdk-var', 'image-builder-path')),
+                'ImageBuilder', self._in_var('sdk-var', 'image-builder-path')),
             {
-                'working-directory': self._in_var_cmd('sdk-var', 'openwrt'),
+                'working-directory': self._in_var('sdk-var', 'openwrt'),
                 'run': '''
-tar -cf tmp.tar bin/targets/*/*/packages
-rm bin/targets/*/*/packages -rf
-rm {sdk} -rf
-rm {ib} -rf
-ls -lh bin/targets/*/*/ || true
-( ls bin/targets/*/*/*.vdi >/dev/null 2>&1 ) && gzip -9n bin/targets/*/*/*.vdi || true
-( ls bin/targets/*/*/*.vmdk >/dev/null 2>&1 ) && gzip -9n bin/targets/*/*/*.vmdk || true
-'''.format(
-                    sdk=self._in_var_cmd('sdk-var', 'sdk-path'),
-                    ib=self._in_var_cmd(
+                    tar -cf tmp.tar bin/targets/*/*/packages
+                    rm bin/targets/*/*/packages -rf
+                    rm {sdk} -rf
+                    rm {ib} -rf
+                    ls -lh bin/targets/*/*/ || true
+                    ( ls bin/targets/*/*/*.vdi >/dev/null 2>&1 ) && gzip -9n bin/targets/*/*/*.vdi || true
+                    ( ls bin/targets/*/*/*.vmdk >/dev/null 2>&1 ) && gzip -9n bin/targets/*/*/*.vmdk || true
+                    '''.format(
+                    sdk=self._in_var('sdk-var', 'sdk-path'),
+                    ib=self._in_var(
                         'sdk-var', 'image-builder-path')
                 )
             },
             self._gen_upload_artifact_step(
-                'Firmware', self._in_var_cmd('sdk-var', 'openwrt') + '/bin/targets'),
+                'Firmware', self._in_var('sdk-var', 'openwrt') + '/bin/targets'),
             {
-                'working-directory': self._in_var_cmd('sdk-var', 'openwrt'),
+                'working-directory': self._in_var('sdk-var', 'openwrt'),
                 'run': '''rm bin/targets -rf\ntar -xf tmp.tar'''
             },
             self._gen_upload_artifact_step(
-                'Packages-base', self._in_var_cmd('sdk-var', 'openwrt') + '/bin'),
+                'Packages-base', self._in_var('sdk-var', 'openwrt') + '/bin'),
             # collect all openwrt's source bundles
             {
                 'if': "steps.cache-openwrt.outputs.cache-hit != 'true'",
